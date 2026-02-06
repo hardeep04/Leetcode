5496be39956dfbf1f1bfbf293f6e19b0a248316f
Hardeep Singh <147726338+hardeep04@users.noreply.github.com>
2026-02-07 03:02:35 +0530

    Time: 3 ms (70.96%), Space: 33 MB (84.88%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     bool isPossible(vector<int>& target) {
-        int n = target.size();
         priority_queue<int> pq(target.begin(), target.end());
-        long long total = accumulate(target.begin(), target.end(), 0);
+        long long total = accumulate(target.begin(), target.end(), 0LL);
         while(pq.top()>1){
-            int top = pq.top();
+            long long top = pq.top();
             pq.pop();
             long long sum = total-top;
             if(top<=sum || sum==0) return 0;
-            int prev = top%sum;
+            long long prev = top%sum;
             if(prev==0) prev=sum;
             pq.push(prev);
             total = sum+prev;
