f75b8941d346bf2e3b1dfe5a620848c92a274266
Hardeep Singh <147726338+hardeep04@users.noreply.github.com>
2026-05-27 22:54:20 +0530

    Time: 0 ms (100%), Space: 18 MB (32.67%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int l=0, r=n-1;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(nums[mid] == target) return 1;
+            if(nums[l] == nums[mid] && nums[mid] == nums[r]){
+                l++;
+                r--;
+                continue;
+            }
+            if(nums[l] <= nums[mid]){
+                if(target >= nums[l] && target <= nums[mid] ) r=mid-1;
+                else l = mid+1;
+            }
+            else{
+                if(target >= nums[mid] && target <= nums[r] ) l = mid+1;
+                else r=mid-1;
+            }
+        }
+        return 0;
+    }
+};
