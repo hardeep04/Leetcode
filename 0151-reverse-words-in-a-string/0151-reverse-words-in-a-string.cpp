f8e09d47c16693e5c4eb7c5d80fc25c7b91b4e74
Hardeep Singh <147726338+hardeep04@users.noreply.github.com>
2026-06-02 20:14:48 +0530

    Time: 2 ms (41.61%), Space: 9.9 MB (87.18%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n= s.length();
         int i1=-1,i2=-1;
-        for(int i=0; i<n; i++){
-            while(i<n && s[i]==' '){
-                s.erase(s.begin()+i);
+        while(s[0]==' ') s.erase(s.begin());
+        while(s.back()==' ') s.pop_back();
+        int n= s.length();
+        for(int i=0; i<n-1; i++){
+            if(s[i]==' ' && s[i+1]==' '){
+                s.erase(s.begin()+i+1);
                 n--;
+                i--;
             }
-            if(i1==-1 && i>0 && i<n && s[i]!=' '){
-                s.insert(s.begin()+i, ' ');
-                i++;
-                n++;
-            }
-            
+        }           
+        for(int i=0; i<n; i++){
             if(i>=n) break;
-            if(i1==-1) i1=i;
+            if(i1==-1 && i==0) i1=i;
+            else if(i1==-1 && i<n-1) i1=i+1;
             if(i2==-1 && (i==n-1 || s[i+1]==' ')){
                 i2=i;
                 reverse(s.begin()+i1, s.begin()+i2+1);
