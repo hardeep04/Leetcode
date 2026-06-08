0027a18d0281b2e8b26fb1e242c1bd77f7838993
Hardeep Singh <147726338+hardeep04@users.noreply.github.com>
2026-06-08 15:40:35 +0530

    Time: 118 ms (45.73%), Space: 8.9 MB (92.51%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     int beautySum(string s) {
-        int n=s.length();
-        long ans=0;
-        vector<vector<int>> freq(n+1, vector<int>(26,0));
-        freq[1][s[0]-'a'] = 1;
-        for(int i=2; i<=n; i++){
-            for(int j=0; j<26; j++){
-                if(j==s[i-1]-'a') freq[i][j] = 1 + freq[i-1][j];
-                else freq[i][j] = freq[i-1][j];
-            }
-        }
-        for(int i=1; i<=n; i++){
-            for(int j=i+1; j<=n; j++){
-                int maxi=-1, mini=INT_MAX;
-                for(int k=0; k<26; k++){
-                    int diff = freq[j][k] - freq[i-1][k];
-                    if(diff > maxi) maxi = diff;
-                    if(diff < mini && diff >0) mini = diff;
+        int ans = 0;
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            int freq[26] = {0};
+            for (int j = i; j < n; j++) {
+                freq[s[j] - 'a']++; 
+                int maxi = 0, mini = INT_MAX;
+                for (int k = 0; k < 26; k++) {
+                    if (freq[k] > 0) {
+                        maxi = max(maxi, freq[k]);
+                        mini = min(mini, freq[k]);
+                    }
                 }
-                ans+=(maxi-mini);
+                ans += (maxi - mini);
             }
         }
+        
         return ans;
     }
 };
