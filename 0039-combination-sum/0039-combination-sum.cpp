a47051e4d9ea5fb4091141053b31a5cc593c31ec
Hardeep Singh <147726338+hardeep04@users.noreply.github.com>
2026-07-02 16:08:19 +0530

    Time: 0 ms (100%), Space: 14.1 MB (57.53%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void fun(vector<int>& v, int target, vector<vector<int>> &ans, vector<int> &temp, int i){
+        if(target<0) return;
+        if(i==v.size()){
+            if(target==0) ans.push_back(temp);
+            return;
+        } 
+        temp.push_back(v[i]);
+        fun(v,target-v[i], ans, temp, i);
+        temp.pop_back();
+        fun(v,target, ans, temp, i+1);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        fun(candidates, target, ans, temp, 0);
+        return ans;
+    }
+};
