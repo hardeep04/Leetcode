7e86a9b2dfcd0b331be5ec9b9652da2ea8680937
Hardeep Singh <147726338+hardeep04@users.noreply.github.com>
2026-07-03 02:39:43 +0530

    Time: 1 ms (22.78%), Space: 9.5 MB (15.09%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    void fun(vector<int> &v, int k, int target, vector<vector<int>>& ans, vector<int> temp, int i){
+    void fun(vector<int> &v, int k, int target, vector<vector<int>>& ans, vector<int> temp, int ind){
         if(target==0 && k==0){
             ans.push_back(temp);
             return;
         }
-        if(target<0 || k==0 || i==9) return;
-        temp.push_back(v[i]);
-        fun(v,k-1, target-v[i], ans, temp, i+1);
-        temp.pop_back();
-
-        fun(v,k, target, ans, temp, i+1);
+        if(target<0 || k==0 || ind==9) return;
+        
+        for(int i=ind; i<9; i++){
+            temp.push_back(v[i]);
+            fun(v,k-1, target-v[i], ans, temp, i+1);
+            temp.pop_back();
+        }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<int> v = {1,2,3,4,5,6,7,8,9}, temp;
