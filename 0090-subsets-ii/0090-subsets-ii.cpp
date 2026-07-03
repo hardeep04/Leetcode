a6d14f91567a666436908edadf4063a38553c6f6
Hardeep Singh <147726338+hardeep04@users.noreply.github.com>
2026-07-03 17:25:47 +0530

    Time: 1 ms (45.45%), Space: 11 MB (22.63%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void fun(vector<int> &arr, vector<vector<int>>& ans, vector<int> temp, int ind){
+        ans.push_back(temp);
+        if(ind==arr.size()){
+            return;
+        }
+        for(int i=ind; i<arr.size(); i++){
+            if(i>ind && arr[i] == arr[i-1]) continue;
+            temp.push_back(arr[i]);
+            fun(arr,ans,temp,i+1);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        vector<int> temp;
+        fun(nums,ans, temp, 0);
+        return ans;
+    }
+};
