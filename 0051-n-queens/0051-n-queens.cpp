3066a8971a8491a571d5d64307ba924f47808c45
Hardeep Singh <147726338+hardeep04@users.noreply.github.com>
2026-07-06 22:01:02 +0530

    Time: 3 ms (62.14%), Space: 10.2 MB (78.92%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,42 +1,33 @@
 class Solution {
 public:
-    bool isvalid(vector<string> &v, int n, int row, int col){
-        for(int i=0; i<row; i++){
-            if(v[i][col] == 'Q') return 0;
-        }
-        for(int i=row,j=col; i>=0 && j>=0; i--,j--){
-            if(v[i][j] == 'Q') return 0;
-        }
-        for(int i=row,j=col; i>=0 && j<n; i--,j++){
-            if(v[i][j] == 'Q') return 0;
-        }
-        return 1;
-    }
-    void solve(vector<vector<string>>& ans, vector<string> &v, int n, int row){
+    void solve(vector<vector<string>>& ans, vector<string> &v, int n, int row, vector<bool>& cols, vector<bool>& diag1, vector<bool>& diag2){
         if(row==n){
             ans.push_back(v);
             return;
         }
-        for(int col=0; col<n; col++){
-            if(isvalid(v,n,row,col)){
-                v[row][col] = 'Q';
-                solve(ans,v,n,row+1);
-                v[row][col] = '.';
+        for (int col = 0; col < n; col++) {
+            if (cols[col] || diag1[row - col + n - 1] || diag2[row + col]) {
+                continue;
             }
+
+            v[row][col] = 'Q';
+            cols[col] = diag1[row - col + n - 1] = diag2[row + col] = 1;
+
+            solve(ans, v, n, row+1, cols, diag1, diag2);
+
+            v[row][col] = '.';
+            cols[col] = diag1[row - col + n - 1] = diag2[row + col] = 0;
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
-        vector<string> v;
+        vector<string> v(n, string(n, '.'));
         
-        for(int i=0; i<n; i++){
-            string s="";
-            for(int j=0; j<n; j++){
-                s+='.';
-            }
-            v.push_back(s);
-        }
-        solve(ans,v,n,0);
+        vector<bool> cols(n, 0);
+        vector<bool> diag1(2 * n - 1, 0);
+        vector<bool> diag2(2 * n - 1, 0);
+        
+        solve(ans,v,n,0, cols, diag1, diag2);
         return ans;
     }
 };
