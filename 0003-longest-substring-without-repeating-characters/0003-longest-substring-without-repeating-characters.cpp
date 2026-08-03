fa526cf91fcee6fb65a4d6ab963c401c7377a1bc
Hardeep Singh <147726338+hardeep04@users.noreply.github.com>
2026-08-03 20:57:23 +0530

    Time: 2750 ms (5%), Space: 19 MB (10.82%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        // int n=s.length();
+        // if(n==0) return 0;
+        // int i=0,j=0, ans=1;
+        // map<char, int> m; 
+        // for(j=0; j<n; j++){
+        //     if(m[s[j]]==1){
+        //         ans = max(ans,j-i);
+        //         while(m[s[j]]!=0){
+        //             m[s[i]]--;
+        //             i++;
+        //         }
+        //     }
+        //     m[s[j]]++;
+        // }
+        // ans = max(ans,j-i);
+        // return ans;
+
+        // // method 2-- instead of storing freq of each char, we store their last seen index
+        
+        
         int n=s.length();
         if(n==0) return 0;
-        int i=0,j=0, ans=1;
-        map<char, int> m;
-        for(j=0; j<n; j++){
-            if(m[s[j]]==1){
-                ans = max(ans,j-i);
-                while(m[s[j]]!=0){
-                    m[s[i]]--;
-                    i++;
-                }
-            }
-            m[s[j]]++;
+        int i=0, ans=1;
+        vector<int> v(256,-1);
+        for(int j=0; j<n; j++){
+            if(v[s[j]]>=i) i=v[s[j]]+1;
+            v[s[j]] = j;
+            ans=max(ans, j-i+1);
+            cout<<s[j]<<" ";
         }
-        ans = max(ans,j-i);
         return ans;
     }
 };
