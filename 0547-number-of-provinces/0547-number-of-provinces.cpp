da4c28dc2edc6784d638b8e77862931bafc4111d
Hardeep Singh <147726338+hardeep04@users.noreply.github.com>
2026-08-05 00:03:40 +0530

    Time: 0 ms (100%), Space: 19.4 MB (71.87%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,39 +1,22 @@
 class Solution {
 public:
-    // void bfs(int n, vector<int> adj[], vector<int>& vis, int node){
-    void bfs(int n, vector<vector<int>>& v, vector<int>& vis, int node){
-        queue<int> q;
-        q.push(node);
+    void dfs(int n, vector<vector<int>>& v, vector<int>& vis, int node){
         vis[node]=1;
-        while(!q.empty()){
-            int curr = q.front();
-            q.pop();
-            for(int i=0; i<n; i++){
-                if(v[curr][i] && !vis[i]){
-                    q.push(i);
-                    vis[i]=1;
-                }
+        for(int i=0; i<n; i++){
+            if(v[node][i]==1 && !vis[i]){
+                dfs(n,v,vis,i);
             }
         }
     }
+    
     int findCircleNum(vector<vector<int>>& v) {
         int n=v.size();
         vector<int> vis(n, 0);
         int ans=0;
-        // vector<int> adj[n];
-        // for(int i=0; i<n; i++){
-        //     for(int j=0; j<n; j++){
-        //         if(i!=j && v[i][j]){
-        //             adj[i].push_back(j);
-        //             adj[j].push_back(i);
-        //         }
-        //     }
-        // }
         for(int i=0; i<n; i++){
             if(!vis[i]){
                 ans++;
-                // bfs(n,adj, vis, i);
-                bfs(n, v, vis, i);
+                dfs(n, v, vis, i);
             }
         }
         return ans;
