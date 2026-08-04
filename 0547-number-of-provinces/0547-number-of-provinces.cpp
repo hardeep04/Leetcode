82001ae9ecf1eb3969d993b33b38523cf4c62cfc
Hardeep Singh <147726338+hardeep04@users.noreply.github.com>
2026-08-04 23:42:43 +0530

    Time: 0 ms (100%), Space: 19.9 MB (47.61%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    void bfs(int n, vector<int> adj[], vector<int>& vis, int node){
+    // void bfs(int n, vector<int> adj[], vector<int>& vis, int node){
+    void bfs(int n, vector<vector<int>>& v, vector<int>& vis, int node){
         queue<int> q;
         q.push(node);
         vis[node]=1;
         while(!q.empty()){
-            int v = q.front();
+            int curr = q.front();
             q.pop();
-            for(auto it : adj[v]){
-                if(!vis[it]){
-                    q.push(it);
-                    vis[it]=1;
+            for(int i=0; i<n; i++){
+                if(v[curr][i] && !vis[i]){
+                    q.push(i);
+                    vis[i]=1;
                 }
             }
         }
@@ -19,19 +20,20 @@ public:
         int n=v.size();
         vector<int> vis(n, 0);
         int ans=0;
-        vector<int> adj[n];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(i!=j && v[i][j]){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
-                }
-            }
-        }
+        // vector<int> adj[n];
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<n; j++){
+        //         if(i!=j && v[i][j]){
+        //             adj[i].push_back(j);
+        //             adj[j].push_back(i);
+        //         }
+        //     }
+        // }
         for(int i=0; i<n; i++){
             if(!vis[i]){
                 ans++;
-                bfs(n,adj, vis, i);
+                // bfs(n,adj, vis, i);
+                bfs(n, v, vis, i);
             }
         }
         return ans;
