2b661784fd12fff6f0e823718372a070e29bb2e2
Hardeep Singh <147726338+hardeep04@users.noreply.github.com>
2026-08-13 00:05:50 +0530

    Time: 10 ms (14%), Space: 18.1 MB (83.13%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> findOrder(int num, vector<vector<int>>& pre) {
+        vector<vector<int>> adj(num);
+        // in-degrees
+        vector<int> indegree(num,0);
+        for(auto &e : pre){
+            adj[e[1]].push_back(e[0]);
+            indegree[e[0]]++;
+        }
+        queue<int> q;
+        for(int i=0; i<num; i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int> ans;
+        int cnt=0;
+        while(!q.empty()){
+            int curr=q.front();
+            q.pop();
+            cnt++;
+            ans.push_back(curr);
+            for(int neighbour : adj[curr]){
+                indegree[neighbour]--;
+                if(indegree[neighbour]==0){
+                    q.push(neighbour);
+                }
+            }
+        }
+        if(cnt==num) return ans;
+        return {};
+    }
+};
