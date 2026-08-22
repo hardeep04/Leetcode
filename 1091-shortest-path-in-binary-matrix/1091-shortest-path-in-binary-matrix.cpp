f1870b1f606f5df0a833e4282527ad64026fcf11
Hardeep Singh <147726338+hardeep04@users.noreply.github.com>
2026-08-22 17:29:01 +0530

    Time: 32 ms (24.2%), Space: 25.8 MB (45.41%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    bool valid(vector<vector<int>>& grid,int n, int r, int c){
-        if(r<0 || c<0 || r>=n || c>=n || grid[r][c]==1) return 0;
+    bool valid(vector<vector<int>>& grid,int n, int r, int c,vector<vector<bool>>& vis){
+        if(r<0 || c<0 || r>=n || c>=n || grid[r][c] || vis[r][c]) return 0;
         return 1;
     }
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
@@ -12,12 +12,13 @@ public:
         vector<int> dr = {-1,-1,0,1,1,1,0,-1};
         vector<int> dc = {0,1,1,1,0,-1,-1,-1};
         vector<vector<int>> dist(n, vector<int>(n,INT_MAX));
-        dist[0][0]=1;
+        vector<vector<bool>> vis(n,vector<bool>(n,0));
+        dist[0][0]=1; vis[0][0]=1;
         while(!q.empty()){
             auto [r,c] = q.front();
             q.pop();
             for(int i=0; i<8; i++){
-                if(valid(grid,n,r+dr[i], c+dc[i]) && dist[r][c] + 1 < dist[r+dr[i]][c+dc[i]]){
+                if(valid(grid,n,r+dr[i], c+dc[i],vis) && dist[r][c] + 1 < dist[r+dr[i]][c+dc[i]]){
                     dist[r+dr[i]][c+dc[i]] = dist[r][c]+1;
                     q.push({r+dr[i], c+dc[i]});
                 }
