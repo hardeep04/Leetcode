7a9c1626d74b810f3141d946f6de032ca082309c
Hardeep Singh <147726338+hardeep04@users.noreply.github.com>
2026-08-22 17:38:41 +0530

    Time: 23 ms (37.57%), Space: 25.1 MB (59.53%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -7,24 +7,25 @@ public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
         if(grid[0][0]) return -1;
-        queue<pair<int,int>> q;
-        q.push({0,0});
+        queue<tuple<int,int,int>> q;
+        q.push(make_tuple(1,0,0));
         vector<int> dr = {-1,-1,0,1,1,1,0,-1};
         vector<int> dc = {0,1,1,1,0,-1,-1,-1};
-        vector<vector<int>> dist(n, vector<int>(n,INT_MAX));
+        // vector<vector<int>> dist(n, vector<int>(n,INT_MAX));
         vector<vector<bool>> vis(n,vector<bool>(n,0));
-        dist[0][0]=1; vis[0][0]=1;
+        // dist[0][0]=1;
+        vis[0][0]=1;
         while(!q.empty()){
-            auto [r,c] = q.front();
+            auto [dis,r,c] = q.front();
             q.pop();
+            if(r==n-1 && c==n-1) return dis;
             for(int i=0; i<8; i++){
-                if(valid(grid,n,r+dr[i], c+dc[i],vis) && dist[r][c] + 1 < dist[r+dr[i]][c+dc[i]]){
-                    dist[r+dr[i]][c+dc[i]] = dist[r][c]+1;
-                    q.push({r+dr[i], c+dc[i]});
+                if(valid(grid,n,r+dr[i], c+dc[i],vis)){
+                    q.push(make_tuple(dis+1,r+dr[i], c+dc[i]));
+                    vis[r+dr[i]][c+dc[i]]=1;
                 }
             }
         }
-        if(dist[n-1][n-1]==INT_MAX) return -1;
-        return dist[n-1][n-1];
+        return -1;
     }
 };
