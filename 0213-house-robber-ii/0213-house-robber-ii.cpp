b51508f67ec67134240852573bb2fe16168f0452
Hardeep Singh <147726338+hardeep04@users.noreply.github.com>
2026-09-01 02:50:00 +0530

    Time: 0 ms (100%), Space: 11 MB (18.78%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
-    int fun(int i, int j, vector<int>& nums, vector<int> &dp){
-        if(i>j) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int take = nums[i] + fun(i+2,j,nums,dp);
-        int nottake = fun(i+1,j,nums,dp);
-        return dp[i] = max(take,nottake);
+    // int fun(int i, int j, vector<int>& nums, vector<int> &dp){
+    //     if(i>j) return 0;
+    //     if(dp[i]!=-1) return dp[i];
+    //     int take = nums[i] + fun(i+2,j,nums,dp);
+    //     int nottake = fun(i+1,j,nums,dp);
+    //     return dp[i] = max(take,nottake);
+    // }
+
+    // tabulation --------------------------------
+    int fun(int s, int e, vector<int>& nums, vector<int> &dp){
+        for(int i=s+1; i<=e; i++){
+            int take = nums[i];
+            if(i>s+1) take += dp[i-2];
+            int nottake = dp[i-1];
+            dp[i] = max(take,nottake);
+        }
+        return dp[e];
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1) return nums[0];
         vector<int> dp(n,-1), dp2(n,-1);
+        dp[0] = nums[0], dp2[1] = nums[1];
         return max(fun(0,n-2, nums, dp), fun(1,n-1, nums, dp2));
     }
 };
