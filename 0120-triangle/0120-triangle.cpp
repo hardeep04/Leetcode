f56a207afac4a3b541f632f2b625982a32a13f73
Hardeep Singh <147726338+hardeep04@users.noreply.github.com>
2026-09-02 03:11:07 +0530

    Time: 0 ms (100%), Space: 13.4 MB (25.72%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    // int fun(vector<vector<int>>& t, int i, int j,vector<vector<int>>& dp){
+    //     if(i == t.size()) return 0;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     int left = t[i][j] + fun(t,i+1, j,dp);
+    //     int right = t[i][j] + fun(t,i+1, j+1,dp);
+    //     return dp[i][j] = min(left, right);
+    // }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        vector<vector<int>> dp(n);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i+1; j++) dp[i].push_back(-1);
+        }
+        // return fun(triangle,0, 0,dp);
+        for(int j=0; j<n; j++){
+            dp[n-1][j] = triangle[n-1][j];
+        }
+        for(int i=n-2; i>=0; i--){
+            for(int j=0; j<i+1; j++){
+                int left = triangle[i][j] + dp[i+1][j];
+                int right = triangle[i][j] + dp[i+1][j+1];
+                dp[i][j] = min(left, right);
+            }
+        }
+        return dp[0][0];
+    }
+};
