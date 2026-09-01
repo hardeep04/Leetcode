ff09fa921564884dbc8d71deff7a6c3c79b84c18
Hardeep Singh <147726338+hardeep04@users.noreply.github.com>
2026-09-02 03:15:51 +0530

    Time: 0 ms (100%), Space: 13.1 MB (64.88%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -9,21 +9,27 @@ public:
     // }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>> dp(n);
-        for(int i=0; i<n; i++){
-            for(int j=0; j<i+1; j++) dp[i].push_back(-1);
-        }
+        // vector<vector<int>> dp(n);
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<i+1; j++) dp[i].push_back(-1);
+        // }
         // return fun(triangle,0, 0,dp);
+        // for(int j=0; j<n; j++){
+        //     dp[n-1][j] = triangle[n-1][j];
+        // }
+        vector<int> prev(n,-1);
         for(int j=0; j<n; j++){
-            dp[n-1][j] = triangle[n-1][j];
+            prev[j] = triangle[n-1][j];
         }
         for(int i=n-2; i>=0; i--){
+            vector<int> curr(n,-1);
             for(int j=0; j<i+1; j++){
-                int left = triangle[i][j] + dp[i+1][j];
-                int right = triangle[i][j] + dp[i+1][j+1];
-                dp[i][j] = min(left, right);
+                int left = triangle[i][j] + prev[j];
+                int right = triangle[i][j] + prev[j+1];
+                curr[j] = min(left, right);
             }
+            prev = curr;
         }
-        return dp[0][0];
+        return prev[0];
     }
 };
