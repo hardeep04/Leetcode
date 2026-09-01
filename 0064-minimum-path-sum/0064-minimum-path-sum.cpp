e1ce5ed1deb9b6b6413d52c243f95af92272f5d8
Hardeep Singh <147726338+hardeep04@users.noreply.github.com>
2026-09-02 01:51:08 +0530

    Time: 0 ms (100%), Space: 16.5 MB (16.17%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
-    int fun(vector<vector<int>>& grid, int i, int j,vector<vector<int>>& dp){
-        int n=grid.size(), m=grid[0].size();
-        if(i>=n || j>=m) return INT_MAX;
-        if(i==n-1 && j==m-1) return grid[i][j];
-        if(dp[i][j]!=-1) return dp[i][j];
-        int left = fun(grid, i+1, j,dp);
-        int right = fun(grid, i, j+1,dp);
-        if(left == INT_MAX && right == INT_MAX) return grid[i][j];
-        return dp[i][j] = grid[i][j] + min(left, right);
-    }
+    // int fun(vector<vector<int>>& grid, int i, int j,vector<vector<int>>& dp){
+    //     int n=grid.size(), m=grid[0].size();
+    //     if(i>=n || j>=m) return INT_MAX;
+    //     if(i==n-1 && j==m-1) return grid[i][j];
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     int left = fun(grid, i+1, j,dp);
+    //     int right = fun(grid, i, j+1,dp);
+    //     if(left == INT_MAX && right == INT_MAX) return grid[i][j];
+    //     return dp[i][j] = grid[i][j] + min(left, right);
+    // }
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size(), m=grid[0].size();
         if(n==1 && m==1) return grid[0][0]; 
         vector<vector<int>> dp(n, vector<int>(m,-1));
-        return fun(grid, 0, 0, dp);
+        // return fun(grid, 0, 0, dp);
+        for(int i=n-1; i>=0; i--){
+            for(int j=m-1; j>=0; j--){
+                if (i == n - 1 && j == m - 1) {
+                    dp[i][j] = grid[i][j];
+                    continue;
+                }
+                int left=0, right=0;
+                if(i<n-1) left += dp[i+1][j];
+                else left=INT_MAX;
+                if(j<m-1) right += dp[i][j+1];
+                else right=INT_MAX;
+                dp[i][j] = grid[i][j] + min(left, right);
+            }
+        }
+        return dp[0][0];
     }
 };
