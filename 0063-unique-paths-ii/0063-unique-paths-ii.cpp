68f4150b080129923d06d765896178f6f6a54b2c
Hardeep Singh <147726338+hardeep04@users.noreply.github.com>
2026-09-01 20:50:39 +0530

    Time: 2 ms (12.71%), Space: 11.5 MB (90.78%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    int fun(int i,int j, int m, int n,vector<vector<int>>& v,vector<vector<int>>& dp){
-        if(i>=m || j>=n || v[i][j]) return 0;
-        if(i==m-1 && j==n-1) return 1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int down = fun(i+1, j, m, n,v,dp);
-        int right = fun(i, j+1, m, n,v,dp);
-        return dp[i][j] = down + right;
-    }
+    // int fun(int i,int j, int m, int n,vector<vector<int>>& v,vector<vector<int>>& dp){
+    //     if(i>=m || j>=n || v[i][j]) return 0;
+    //     if(i==m-1 && j==n-1) return 1;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     int down = fun(i+1, j, m, n,v,dp);
+    //     int right = fun(i, j+1, m, n,v,dp);
+    //     return dp[i][j] = down + right;
+    // }
     int uniquePathsWithObstacles(vector<vector<int>>& v) {
         int m=v.size(), n=v[0].size();
         if(v[0][0]) return 0;
-        vector<vector<int>> dp(m, vector<int>(n,-1));
-        bool flag=0;
-        for(int i=0; i<n; i++){
-            if(v[0][i]) flag = 1;
-            if(!flag) dp[0][i] = 1;
-            else dp[0][i]=0;
-        } 
-        flag=0;
-        for(int i=0; i<m; i++){
-            if(v[i][0]) flag = 1;
-            if(!flag) dp[i][0] = 1;
-            else dp[i][0]=0;
+        vector<int> prevRow(n);
+        prevRow[0]=1;
+        for(int i=1; i<n; i++){
+            if(v[0][i]) prevRow[i]=0;
+            else prevRow[i] = prevRow[i-1];
         }
         for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                if(v[i][j]) dp[i][j]=0;
-                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            vector<int> currRow(n);
+            for(int j=0; j<n; j++){
+                if(v[i][j]) currRow[j]=0;
+                else if(j==0) currRow[j] = prevRow[j];
+                else currRow[j] = prevRow[j] + currRow[j-1];
             }
+            prevRow = currRow;
         }
-        return dp[m-1][n-1];
+        return prevRow[n-1];
     }
 };
