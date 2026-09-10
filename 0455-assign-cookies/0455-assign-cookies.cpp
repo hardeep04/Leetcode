933a8215cd07cf52fbd2d5558a21b087053b69cd
Hardeep Singh <147726338+hardeep04@users.noreply.github.com>
2026-09-10 20:14:49 +0530

    Time: 3 ms (94.71%), Space: 44.9 MB (47.94%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -3,13 +3,13 @@ public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         sort(g.begin(), g.end());
         sort(s.begin(), s.end());
-        int gs=g.size(), ss=s.size(), j=0, ans=0;
-        for(int i=0; i<gs; i++){
+        int gs=g.size(), ss=s.size();
+        int i=0, j=0;
+        for(i=0; i<gs; i++){
             while(j<ss && s[j]<g[i]) j++;
-            if(j<ss && s[j]>=g[i]){
-                ans++; j++;
-            }
+            if(j==ss) break;
+            if(j<ss && s[j]>=g[i]) j++;
         }
-        return ans;
+        return i;
     }
 };
