4bada963f0dcf5ae78aed5d25cf93a0a05807bb0
Hardeep Singh <147726338+hardeep04@users.noreply.github.com>
2026-09-11 16:08:09 +0530

    Time: 3 ms (30%), Space: 16.2 MB (61.78%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,37 +1,18 @@
 class Solution {
 public:
-    // int fun(vector<vector<int>>& grid, int i, int j,vector<vector<int>>& dp){
-    //     int n=grid.size(), m=grid[0].size();
-    //     if(i>=n || j>=m) return INT_MAX;
-    //     if(i==n-1 && j==m-1) return grid[i][j];
-    //     if(dp[i][j]!=-1) return dp[i][j];
-    //     int left = fun(grid, i+1, j,dp);
-    //     int right = fun(grid, i, j+1,dp);
-    //     if(left == INT_MAX && right == INT_MAX) return grid[i][j];
-    //     return dp[i][j] = grid[i][j] + min(left, right);
-    // }
+    int fun(vector<vector<int>>& grid, int i, int j, vector<vector<int>>& dp){
+        int n=grid.size(), m=grid[0].size();
+        if(i>=n || j>=m) return 1e6;
+        if(i==n-1 && j==m-1) return grid[i][j];
+        if(dp[i][j]!=-1) return dp[i][j];
+        int d = grid[i][j] + fun(grid, i+1, j, dp);
+        int r = grid[i][j] + fun(grid, i, j+1, dp);
+        if(d == 1e6 && r == 1e6) return grid[i][j];
+        return dp[i][j] = min(d,r);
+    }
     int minPathSum(vector<vector<int>>& grid) {
         int n=grid.size(), m=grid[0].size();
-        if(n==1 && m==1) return grid[0][0]; 
-        // vector<vector<int>> dp(n, vector<int>(m,-1));
-        // return fun(grid, 0, 0, dp);
-        vector<int> prev(m);
-        for(int i=n-1; i>=0; i--){
-            vector<int> curr(m);
-            for(int j=m-1; j>=0; j--){
-                if (i == n - 1 && j == m - 1) {
-                    curr[j] = grid[i][j];
-                    continue;
-                }
-                int left=0, right=0;
-                if(i<n-1) left += prev[j];
-                else left=INT_MAX;
-                if(j<m-1) right += curr[j+1];
-                else right=INT_MAX;
-                curr[j] = grid[i][j] + min(left, right);
-            }
-            prev=curr;
-        }
-        return prev[0];
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return fun(grid, 0, 0, dp);
     }
 };
