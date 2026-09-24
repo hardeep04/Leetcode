0cee67a09eb1111d319f86dbdb284bd323bca41c
Hardeep Singh <147726338+hardeep04@users.noreply.github.com>
2026-09-24 19:52:45 +0530

    Time: 0 ms (100%), Space: 19.5 MB (11.89%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.py
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -2,8 +2,5 @@ class Solution:
     def maximumWealth(self, v: list[list[int]]) -> int:
         maxi=0
         for i in range(len(v)):
-            temp=0
-            for j in range(len(v[0])):
-                temp+=v[i][j]
-            maxi = max(maxi, temp)
+            maxi = max(maxi, sum(v[i]))
         return maxi
